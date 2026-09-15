071470fce725fac63029172bb2f84e0f53f288f3
JaiderSoto07 <jaider.soto0137@uco.net.co>
2026-09-14 22:51:56 -0500

    Prueba: primer cambio en Main

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and Welcome! Mi primer cambio.");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -12,4 +13,5 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+
 }
